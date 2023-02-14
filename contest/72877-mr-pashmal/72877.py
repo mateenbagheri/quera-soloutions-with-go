386db1a77e9184f118b7d93d0a27681ae1c09bf5
Mateen Bagheri Asl <mateenbagheri@gmail.com>
2023-02-14 12:50:45 +0330

    added python version of mr pashmal since there was no go upload option

---
+++ contest/72877-mr-pashmal/72877.py
@@ -0,0 +1,7 @@
+def main():
+    all_input = input()
+    input_list = all_input.split(" ")
+    print(int(int(input_list[0]) * float(input_list[1]) * int(input_list[2])))
+
+if __name__ == "__main__":
+    main()
